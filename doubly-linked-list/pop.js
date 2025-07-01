22fb32453f464d34ac72044f084442cd33d0ff99
Vinicius Guterres <64093692+ViniciusGuterres@users.noreply.github.com>
2025-07-01 12:28:40 -0300

    Create pop.js

---
+++ doubly-linked-list/pop.js
@@ -0,0 +1,73 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+
+    setNext(node) {
+        this.next = node;
+    }
+
+    setPrev(node) {
+        this.prev = node;
+    }
+}
+
+class DoublyLinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+
+        this.head = newNode;
+        this.tail = newNode;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+
+        if (this.head == null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.setNext(newNode);
+            newNode.setPrev(this.tail);
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
+
+    pop() {
+        if (this.head == null) return null;
+
+        if (this.head?.next != null) {
+            let tmpTail = this.tail;
+            this.tail = tmpTail.prev;
+            this.tail.setNext(null);
+            this.length--;
+
+            return tmpTail;
+        }
+
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+
+        return null;
+    }
+}
+
+let myDoublyLinkedList = new DoublyLinkedList(1);
+myDoublyLinkedList.push(2);
+myDoublyLinkedList.push(3);
+myDoublyLinkedList.push(20);
+
+console.log('pop: ', myDoublyLinkedList.pop())
+// console.log('pop: ', myDoublyLinkedList.pop())
+// console.log('pop: ', myDoublyLinkedList.pop())
+
+
+console.log("🚀 ~ myDoublyLinkedList:", myDoublyLinkedList)
